079f90acef595ef0d648c1c934e8153d71222c1e
sunghogo <70732249+sunghogo@users.noreply.github.com>
2023-08-08 14:42:02 -0400

    Lonely Integer: 100.0

---
+++ Lonely Integer.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'lonelyinteger' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+def lonelyinteger(a):
+    # Write your code here
+    dupe_dict = {}
+    for num in a:
+        if num in dupe_dict:
+            dupe_dict.pop(num)
+        else:
+            dupe_dict[num] = 0
+    for key in dupe_dict:
+        return key
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = lonelyinteger(a)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
