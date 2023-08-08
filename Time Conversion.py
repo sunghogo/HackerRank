41188ce5d9d695549fd514e0ed65b912de170b75
sunghogo <70732249+sunghogo@users.noreply.github.com>
2023-08-08 14:03:38 -0400

    Time Conversion: 100.0

---
+++ Time Conversion.py
@@ -0,0 +1,32 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+from datetime import datetime
+
+#
+# Complete the 'timeConversion' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING s as parameter.
+#
+
+def timeConversion(s):
+    # Write your code here
+    datetime_obj = datetime.strptime(s, "%I:%M:%S%p")
+    military_time = datetime.strftime(datetime_obj, "%H:%M:%S")
+    return(military_time)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = timeConversion(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
